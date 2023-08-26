b365013c11fc04e0d5b871bbb247f9028b045d38
RoyalAttacker <alihamza173oo3@gmail.com>
2023-08-26 12:28:09 +0500

    changed index files

--- index.js
+++ index.js
@@ -3,10 +3,9 @@ document.body.style.backgroundColor = 'pink';
 /*Question 1st*/
 //programm to add first n even numbers
 let sum = 0;
-let even = 2;
 const sumEvens = (n) => {
     for (let index = 1; index <= n; index++) {
-        sum = sum + (even * index);
+        sum = sum + (2 * index);
     }
     return sum;
 };
